0f6cdc3ac3e76d55b53a0f587f26b2dcddb4db8e
Dev1234 <33207538+Dev1234@users.noreply.github.com>
2018-08-27 16:40:07 -0500

    Added company name.

--- src/main/java/com/example/demo/KeyStats.java
+++ src/main/java/com/example/demo/KeyStats.java
@@ -5,19 +5,27 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class KeyStats {
 	private Long marketcap;
+	private String companyName;
 	
 	public Long getmarketcap(){
 		return marketcap;
 	}
+	public String getcompanyName(){
+		return companyName;
+	}
 	
 	public void setmarketcap(Long marketcap){
 		this.marketcap = marketcap;
+	}
+	public void setcompanyName(String companyName){
+		this.companyName = companyName;
 	}
 	
 	@Override
     public String toString() {
         return "Watchlist{" +
         		"Market Cap=" + marketcap  +
+        		"Company Name" + companyName +
                 '}';
     }
 
